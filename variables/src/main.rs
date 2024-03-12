2a831defbb6caad0c214ef316beafb92a23a013e
Rofix <frankiewb9527@gmail.com>
2024-03-12 13:43:47 +0800

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,22 @@
+use std::io;
+
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    let one = tup.0;
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of theelement at index {index} is: {element}");
 }
