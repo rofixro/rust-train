24e4ef4672e3480a40c2dfc3a398a281242a2b35
Rofix <frankiewb9527@gmail.com>
2024-03-12 13:26:56 +0800

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-
-    println!("The value of x is: {x}");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
 }
