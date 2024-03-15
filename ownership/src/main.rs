c144f9067a086592819d798e29385d2973984fe6
Rofix <frankiewb9527@gmail.com>
2024-03-15 23:49:53 +0800

    init ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let s = String::from("hello");
+
+    takes_ownership(s);
+
+    println!("{}", s);
+
+    let x = 5;
+
+    makes_copy(x);
+
+    println!("{}", x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
