536a287c5a35ec2a7f4de0c0728b7dc738b822e8
Rofix <frankiewb9527@gmail.com>
2024-03-16 11:07:01 +0800

    update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,21 +1,11 @@
 fn main() {
-    let s = String::from("hello");
+    let s1 = String::from("hello");
 
-    takes_ownership(s);
+    let len = calculate_length(&s1);
 
-    println!("{}", s);
-
-    let x = 5;
-
-    makes_copy(x);
-
-    println!("{}", x);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
-}
-
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
