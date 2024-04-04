6312fad99b8d46da9828bd5e4d0fe17bb1496bf8
Rofix <frankiewb9527@gmail.com>
2024-04-04 18:26:30 +0800

    update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
-    let mut s1 = String::from("hello");
+    let mut s = String::from("hello world");
 
-    let r1 = &s1;
-    let r2 = &s1;
-    println!("{}, {}", r1, r2);
+    let word = first_word(&s);
 
-    let r3 = &mut s1;
-    println!("{}", r3);
+    s.clear();
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
