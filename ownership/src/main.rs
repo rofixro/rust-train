784037f1776e47e7a249479dcf8d9c6102216fd5
Rofix <frankiewb9527@gmail.com>
2025-01-30 23:31:23 +0800

    update ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,11 @@
 // 在 rust 中，对于某一个值。当拥有它的变量离开作用域范围时，内存会立即被释放。
 
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = s1.clone();
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+}
 
-    println!("s1: {}, s2: {}", s1, s2);
+fn calculate_length(s: String) -> (String, usize) {
+    let len = s.len();
+    (s, len)
 }
