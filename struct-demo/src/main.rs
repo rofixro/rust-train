afc1dc591d48bb378f08a82bb9e7a27677b6d3d5
Rofix <frankiewb9527@gmail.com>
2025-01-31 23:42:52 +0800

    update struct

--- struct-demo/src/main.rs
+++ struct-demo/src/main.rs
@@ -1,10 +1,35 @@
 // struct 一旦声明是可变的，那么其中所有的变量都是可变的
 
+// tuple struct
+// struct Color(i32, i32, i32);
+
+// let black = Color(0, 0, 0);
+
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
+}
+
+// impl 关联的方法可以这样写,以便更好的组织代码
+struct Rectangle {
+    width: u32,
+    length: u32,
+}
+
+// 方法
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
+    // 关联函数
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            length: size,
+        }
+    }
 }
 
 fn main() {
@@ -17,14 +42,18 @@ fn main() {
         email: String::from("abc@gmail.com"),
     };
 
-    let user2  = User {
+    let user2 = User {
         active: false,
         sign_in_count: 666,
         ..user1
-    }
+    };
+
+    let rect = Rectangle {
+        width: 30,
+        length: 50,
+    };
+
+    let s = Rectangle::square(20);
+
+    println!("{}", rect.area());
 }
-
-// tuple struct
-struct Color(i32, i32, i32);
-
-let black = Color(0, 0, 0);
